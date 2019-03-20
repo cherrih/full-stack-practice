810bf12d7be0dd27986713719575dfeceaaf342a
cherrih <cherri.hartigan@gmail.com>
2019-03-19 17:28:15 -0700

    Complete initial setup

--- helpers/poke.js
+++ helpers/poke.js
@@ -1,25 +1,14 @@
 const request = require('request');
 const config = require('../config.js');
 
-let getReposByUsername = (searchTerm) => {
+let getPokemon = (searchTerm) => {
   // TODO - Use the request module to request info about pokemon
   // from the pokemon api
   // https://pokeapi.co/docs/v2.html#pokemon
 
   // The options object has been provided to help you out, 
   // but you'll have to fill in the URL
-  let options = {
-    url: `https://pokeapi.co/api/v2/${searchTerm}`
-  };
-
-  request(options, (err, res, body) => {
-    if (err) {
-      callback(err);
-    } else {
-      var info = JSON.parse(body);
-      callback(null, info);
-    }
-  });
+  
 }
 
-module.exports.getReposByUsername = getReposByUsername;
+module.exports.getPokemon = getPokemon;
